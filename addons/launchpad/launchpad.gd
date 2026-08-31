df576a53207f4c9e60425feccdba1ef2e327a121
vaner-org <9658355+vaner-org@users.noreply.github.com>
2026-09-01 04:32:42 +0530

    Timed press

--- addons/launchpad/launchpad.gd
+++ addons/launchpad/launchpad.gd
@@ -5,6 +5,13 @@ var is_editor_focused = false
 
 # You can change this to any button
 var button: JoyButton = JOY_BUTTON_GUIDE
+# Change this to disable running Launchpin scene
+var use_launchpin: bool = true
+
+var guide_pressed: bool = false
+var guide_press_time: int = 0
+var guide_cancelled: bool = false
+const GUIDE_MAX_HOLD_MSEC: int = 500
 
 func _enter_tree():
 	is_editor_focused = true
@@ -17,16 +24,50 @@ func _exit_tree():
 		get_tree().get_root().disconnect("focus_exited", _on_editor_focus_exited)
 
 func _input(event):
-	if event is InputEventJoypadButton and event.pressed and event.button_index == button:
-		if is_editor_focused:
-			if Engine.has_singleton("Launchpin"):
-				Engine.get_singleton("Launchpin").run_scene()
-			else:
-				print("Launching game...")
-				run_project()
+	if not (event is InputEventJoypadButton):
+		return
+
+	# Track the guide button's own press/release
+	if event.button_index == button:
+		if event.is_pressed():
+			guide_pressed = true
+			guide_cancelled = false
+			guide_press_time = Time.get_ticks_msec()
+			return
+
+		if event.is_released():
+			if not guide_pressed:
+				return
+
+			var held_msec = Time.get_ticks_msec() - guide_press_time
+			var was_cancelled = guide_cancelled
+
+			# reset state for next press
+			guide_pressed = false
+			guide_cancelled = false
+
+			if was_cancelled:
+				return
+			if held_msec >= GUIDE_MAX_HOLD_MSEC:
+				return
+
+			_do_guide_action()
+		return
+
+	# Any other JOY_BUTTON press while guide button is held down cancels it
+	if guide_pressed and event.is_pressed():
+		guide_cancelled = true
+
+func _do_guide_action():
+	if is_editor_focused:
+		if use_launchpin and Engine.has_singleton("Launchpin"):
+			Engine.get_singleton("Launchpin").run_scene()
 		else:
-			print("Exiting game...")
-			quit_game()
+			print("Launching game...")
+			run_project()
+	else:
+		print("Exiting game...")
+		quit_game()
 
 func _on_editor_focus_entered():
 	is_editor_focused = true
